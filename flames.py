8d7a0d2d128dbb67c503f761bed0bf12f3f7407d
Sri Vinishruthan A <srivinishruthan.a@hcltech.com>
2026-04-16 17:03:21 +0530

    the code is in a function

--- flames.py
+++ flames.py
@@ -1,15 +1,18 @@
+def flames(first_name,second_name):
+    leng=0
+    for i in first_name:
+        for j in second_name:
+            if i != j:
+                leng=leng+1
+
+    flame=["F - Friends","L - Love","A - Affection","M - Marriage","E - Enemy","S - Siblings"]
+    div = leng % 6
+    return(flame[div])
+
 first_name=list(input("Enter your first name: ").split(" "))
 first_name="".join(first_name)
 second_name=list(input("Enter your second name: ").split(" "))
 second_name="".join(second_name)
-leng=0
 
-for i in first_name:
-    for j in second_name:
-        if i != j:
-            leng=leng+1
-
-flame=["F - Friends","L - Love","A - Affection","M - Marriage","E - Enemy","S - Siblings"]
-div = leng % 6
-print(flame[div])
-
+flames_obj = flame(first_name,second_name)
+print(flames_obj) 
